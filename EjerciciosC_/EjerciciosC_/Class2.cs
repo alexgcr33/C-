14ba9382038e3e493b9cba4573401970c3c2b960
Unknown <alexgcr33@gmail.com>
2017-10-31 09:04:56 +0100

    C#

---
+++ EjerciciosC_/EjerciciosC_/Class2.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosC_
+{
+     class Menu
+    {
+        private string[] posibles = null;
+        private bool exit = false;
+        private int sel = 0;
+        
+
+        public Menu(string[] posibles)
+        {
+            this.posibles = posibles;
+        }
+
+        public bool cerrar() { return exit; }
+
+        public void muestra()
+        {
+            Console.WriteLine(" Escoge entre las opciones!!");
+            for (int i = 0; i < posibles.Length; i++)
+            {
+
+                Console.WriteLine(i +1+ " " + posibles[i]);
+
+               
+            }
+            this.sel = Int32.Parse(Console.ReadLine());
+        }
+
+        public void select()
+        {
+            switch (this.sel)
+            {
+                case 1:
+                    double real = 764.7834;
+                    Console.WriteLine(" Te muestro por pantalla el numero " + Truncar(real) );
+                    break;
+
+                case 2:
+                    double real1 = 764.7834;
+                    int entero =  0 ;
+                    TruncarV2(real1, ref entero);
+                    Console.WriteLine(" Te muestro el entero " + entero );
+                    break;
+
+                case 3:
+                    double real2 = 764.7834;
+                    Console.WriteLine(" Te muestro el numero Truncado " + TruncarV3(ref real2));                     
+                    break;
+
+                case 4:
+                    double real3 = 764.7834;
+                    int redon = 2;
+                    Console.WriteLine(" Te muestro el numero redondeado con 2 decimales " + TruncarV4(real3,ref redon));
+                    break;
+
+                case 5:
+                    double real4 = 764.7834;
+                    int redond = 3;
+                    double conten = 0;
+                    Console.WriteLine("El numero redondeado te lo paso a otro entero " + TruncarV5(real4, redond, conten));
+                    break;
+
+                case 6:
+                    double real5 = 764.7834;
+                    int escrito =0 ;
+                    Console.WriteLine(" ¿Cuantos decimales quieres? (MAX 3) ");
+                    escrito = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("El numero redondeado por los decimales que has querido " + TruncarV6(real5,escrito ));
+                    break;
+
+                case 7:
+                    this.exit = false;
+                    break;
+            }
+        }
+           
+         public double Truncar(double numero)
+        {
+
+            return (int)numero;
+        }
+
+        public double TruncarV2(double numero, ref int entero)
+        {
+            return entero = (int)numero;
+
+        }   
+         
+        public double TruncarV3( ref double numero)
+        {
+            return numero = Math.Truncate(numero);
+
+        }
+        
+        public double TruncarV4(double numero, ref int redon)
+        {
+
+            return Math.Round(numero, redon);
+        }
+
+        public double TruncarV5(Double numero, int redon, double contenedor) {
+
+            return contenedor = Math.Round(numero, redon);
+            }
+
+        public double TruncarV6(double numero, int elec)
+        {
+            return numero = Math.Round(numero, elec);
+        }
+
+        }
+
+
+        
+        
+
+
+
+        }
+
+
+        
+            
+
+        
+
+        
+
+        
+    
+    
+        
+    
+
+
+
---
+++ EjerciciosC_/EjerciciosC_/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosC_
+{
+    class Program
+    {
+        static void Main(string[] argv)
+        {
+            double num1= 764.7834;
+            double num2 = 0;
+            int num3 = 0;
+            
+            string[] posibles = {
+                "Dame un real y devuelve un numero sin decimales",
+                "Dame un real y entero", "Dar un real ", "Dar real y entero ",
+                "Dar real, entero y real ", "Dar real y entero " , "Salir"
+            };
+           
+
+            Menu menu = new Menu(posibles);
+           
+            do
+            {
+                Console.Clear();
+                menu.muestra();
+                menu.select();
+
+                Console.ReadKey();
+
+
+            } while (menu.cerrar() != false); 
+
+            //Console.ReadKey();
+
+            
+        }
+
+    }
+
+}
