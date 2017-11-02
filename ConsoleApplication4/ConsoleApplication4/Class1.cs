c8e735806ef86906babef25e4bc6a5ecfa82ecc8
Unknown <alexgcr33@gmail.com>
2017-11-02 18:35:52 +0100

    Ejercicio Fechas

---
+++ ConsoleApplication4/ConsoleApplication4/Class1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Fecha
+    {
+        private int dia;
+        private int mes;
+        private int anio;
+        private string intro;
+        #region publicas
+        public Fecha(int d, int m, int a, string intr)
+        {
+            this.dia = d;
+            this.mes = m;
+            this.anio = a;
+            this.intro = intr;
+        }
+
+
+        public Fecha(string fecha)
+        {
+            this.intro = fecha;
+            ImportarFechaCadena(intro);
+
+        }
+        public int getAnio()
+        {
+            return this.anio;
+        }
+
+        public int getMes()
+        {
+
+            return this.mes;
+        }
+
+        public int getDia()
+        {
+            return this.dia;
+        }
+        public int setDia()
+        {
+            return this.dia;
+        }
+        public int setMes()
+        {
+            return this.mes;
+        }
+
+        //internal void ToString(string v)
+        //{
+          //  throw new NotImplementedException();
+        //}
+
+        public int setAnio()
+        {
+            return this.anio;
+        }
+
+        public Boolean AnioBisiesto()
+        {
+
+            if (this.anio % 4 == 0)
+            {
+                return true;
+
+            }
+
+            return false;
+        }
+
+
+        public String ACadena()
+        {
+            return dia.ToString("00") + "/" + mes.ToString("00") + "/" + anio.ToString("0000");  
+
+        }
+
+        public Fecha AumentarMes()
+        {
+
+           
+
+            Fecha f = new Fecha(this.ACadena());
+            f.AumentoMes();
+            return f;
+        }
+
+        public Boolean EsMayor(Fecha fx)
+        {
+           if (this.anio > fx.getAnio())
+            {
+                return true;
+            }else if(this.anio == fx.getAnio())
+             {
+                if(this.mes > fx.getMes())
+                {
+                    return true;
+
+
+                }else if(this.mes == fx.getMes())
+                {
+                    if(this.dia == fx.getDia())
+                    {
+                        return true;
+
+                    }else if(this.dia == fx.getDia())
+                    {
+                        return false;
+
+                    }
+
+                }
+
+            }
+
+            return false;
+            
+        }
+
+        #endregion
+
+        #region privadas
+        private void ImportarFechaCadena(string fecha)
+        {
+            string[] trozos = fecha.Split('/');
+            this.dia = int.Parse(trozos[0]);
+            this.mes  = int.Parse(trozos[1]);
+            this.anio = int.Parse(trozos[2]);
+
+
+            }
+
+        private void AumentoMes()
+        {
+            if (mes == 12)
+            {
+                mes = 1;
+                anio++;
+            }
+            else
+
+            mes++;
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,166 @@
+﻿using ConsoleApplication4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fecha_ejer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sel = 0;
+
+            do
+            {
+                Console.WriteLine("Bienvenido a las fechas, escoge una opcion");
+                Console.WriteLine("1.Sumar Mes");
+                Console.WriteLine("2.Si es bisiesto");
+                Console.WriteLine("3.Comparar 3 fechas");
+                Console.WriteLine("4.Salir");
+
+                sel = Int32.Parse(Console.ReadLine());
+
+                switch (sel)
+                {
+                    case 1:
+                        {
+
+                            string fex = null;
+                            Console.WriteLine("Dame una fecha con el formato DD/MM/AAAA");
+                            fex = (Console.ReadLine());
+
+
+                            Fecha f1 = new Fecha(fex);
+
+
+
+                            // f1.ACadena();
+                            f1 = f1.AumentarMes();
+                            Console.WriteLine(f1.ACadena());
+                            Console.ReadKey();
+                            Console.Clear();
+
+
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            string flex = null;
+                            Console.WriteLine("Dame una Fecha: (Formato DD/MM/AAAA)");
+                            flex = (Console.ReadLine());
+
+                            Fecha f1 = new Fecha(flex);
+
+                            if (f1.AnioBisiesto() == true)
+                            {
+                                Console.WriteLine("El año es bisiesto");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No es bisiesto");
+                            }
+
+                            Console.ReadKey();
+                            Console.Clear();
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            string flex1 = null;
+                            string flex2 = null;
+                            string flex3 = null;
+                            Console.WriteLine("Dame una Fecha para 1: (Formato DD/MM/AAAA)");
+                            flex1 = Console.ReadLine();
+                            Console.WriteLine("Dame una Fecha para 2: (Formato DD/MM/AAAA)");
+                            flex2 = Console.ReadLine();
+                            Console.WriteLine("Dame una Fecha para 3: (Formato DD/MM/AAAA)");
+                            flex3 = Console.ReadLine();
+
+                            Console.Clear();
+                            Fecha f1 = new Fecha(flex1);
+                            Fecha f2 = new Fecha(flex2);
+                            Fecha f3 = new Fecha(flex3);
+
+                            if (f1.EsMayor(f2) == true)
+                            {
+                                if (f1.EsMayor(f3) == true) ;
+                                if (f2.EsMayor(f3) == true)
+                                {
+                                    Console.WriteLine(f1.ACadena());
+                                    Console.WriteLine(f2.ACadena());
+                                    Console.WriteLine(f3.ACadena());
+                                }
+                                else
+                                {
+                                    Console.WriteLine(f1.ACadena());
+                                    Console.WriteLine(f3.ACadena());
+                                    Console.WriteLine(f2.ACadena());
+                                }
+
+                            }
+                            else if (f2.EsMayor(f3) == true)
+                            {
+                                if (f3.EsMayor(f1) == true)
+                                {
+                                    Console.WriteLine(f2.ACadena());
+                                    Console.WriteLine(f3.ACadena());
+                                    Console.WriteLine(f1.ACadena());
+                                }
+                                else
+                                {
+                                    Console.WriteLine(f2.ACadena());
+                                    Console.WriteLine(f1.ACadena());
+                                    Console.WriteLine(f3.ACadena());
+
+                                }
+                            }
+
+                                if (f2.EsMayor(f1))
+                                {
+                                    Console.WriteLine(f3.ACadena());
+                                    Console.WriteLine(f2.ACadena());
+                                    Console.WriteLine(f1.ACadena());
+                                }
+                                else
+                                {
+                                    Console.WriteLine(f3.ACadena());
+                                    Console.WriteLine(f1.ACadena());
+                                    Console.WriteLine(f2.ACadena());
+                                }
+
+
+                                
+                        }
+                                Console.ReadKey();
+                        Console.Clear();
+                                break;
+                            
+                    case 4:
+                        {
+                            break;
+                        }
+                    default: { break; }
+
+                }
+
+
+
+
+
+
+            } while (sel != 4);
+
+
+
+
+
+
+        }
+    }
+}
